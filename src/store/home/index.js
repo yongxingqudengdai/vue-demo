21ec2b3f9630e022895c377caf04fe3783e74494
xklld <ibgallery200504@gmail.com>
2023-04-06 15:01:36 +0800

    一级标题mouseenter、mouseleave设置蓝色背景

--- src/store/home/index.js
+++ src/store/home/index.js
@@ -16,14 +16,13 @@ const mutations = {
   },
   CATEGORYLIST(state,value){
     state.categoryList = value;
-    console.log(value)
+    // console.log(value)
   }
 };
 const actions = {
   // 通过api获取服务器数据
   async categoryList(context){
     let result = await reqCategoryList();
-    console.log(result);
     if(result.code === 200){
       // 执行mutation方法
       context.commit('CATEGORYLIST',result.data);
